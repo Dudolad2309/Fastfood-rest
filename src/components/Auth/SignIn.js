3051e7994bc25216e56d9caaac88a0dce0f9904f
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 12:29:52 +0300

    commit SignIn

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import Login from './Login';
 import firebase from 'firebase/app';
-import { firebaseApp } from '../../base';
+import {firebaseApp} from '../../base';
 
 class SignIn extends React.Component {
-  state = {
-    user: ''
-  };
+	state = {
+		user: ''
+	};
 
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.authHandler({ user });
-      }
-    });
-  }
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.authHandler({user});
+			}
+		});
+	}
 
-  authHandler = async authData => {
-    const { email } = authData.user;
-    this.setState({ user: email });
-  };
+	authHandler = async authData => {
+		const {email} = authData.user;
+		this.setState({user: email});
+	};
 
-  authenticate = () => {
-    const authProvider = new firebase.auth['GithubAuthProvider']();
-    firebaseApp
-      .auth()
-      .signInWithPopup(authProvider)
-      .then(this.authHandler);
-  };
+	authenticate = () => {
+		const authProvider = new firebase.auth['GithubAuthProvider']();
+		firebaseApp
+			.auth()
+			.signInWithPopup(authProvider)
+			.then(this.authHandler);
+	};
 
-  render() {
-    if (!this.state.user) {
-      return <Login authenticate={this.authenticate} />;
-    }
-    return this.props.children;
-  }
+	render() {
+		if (!this.state.user) {
+			return <Login authenticate={this.authenticate}/>;
+		}
+		return this.props.children;
+	}
 }
 
 export default SignIn;
