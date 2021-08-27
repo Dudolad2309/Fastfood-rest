7812646840bcdf545d2613cd9e9056ba70b85de9
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 12:38:20 +0300

    commit MenuAdmin

--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -5,66 +5,66 @@ import EditBurgerForm from './EditBurgerForm';
 import firebase from 'firebase/app';
 
 class MenuAdmin extends React.Component {
-  state = {
-    photo: '',
-    user: ''
-  };
+	state = {
+		photo: '',
+		user: ''
+	};
 
-  static propTypes = {
-    burgers: PropTypes.object,
-    deleteBurger: PropTypes.func,
-    updateBurger: PropTypes.func,
-    addBurger: PropTypes.func,
-    loadSampleBurgers: PropTypes.func
-  };
+	static propTypes = {
+		burgers: PropTypes.object,
+		deleteBurger: PropTypes.func,
+		updateBurger: PropTypes.func,
+		addBurger: PropTypes.func,
+		loadSampleBurgers: PropTypes.func
+	};
 
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.authHandler({ user });
-      }
-    });
-  }
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.authHandler({user});
+			}
+		});
+	}
 
-  authHandler = async authData => {
-    const { email, photoURL } = authData.user;
-    this.setState({ user: email, photo: photoURL });
-  };
+	authHandler = async authData => {
+		const {email, photoURL} = authData.user;
+		this.setState({user: email, photo: photoURL});
+	};
 
-  render() {
-    const { user, photo } = this.state;
-    const avatar = photo ? photo : '/images/avatar.png';
-    return (
-      <div className='menu-admin'>
-        {user ? (
-          <div className='login-header'>
-            <div className='avatar'>
-              <img src={avatar} alt={user} />
-            </div>
-            <button className='buttonLogout' onClick={this.props.handleLogout}>
-              Выйти
-            </button>
-          </div>
-        ) : null}
-        <h2>Управление Меню</h2>
-        {Object.keys(this.props.burgers).map(key => {
-          return (
-            <EditBurgerForm
-              key={key}
-              index={key}
-              deleteBurger={this.props.deleteBurger}
-              updateBurger={this.props.updateBurger}
-              burger={this.props.burgers[key]}
-            />
-          );
-        })}
-        <AddBurgerForm addBurger={this.props.addBurger} />
-        <button onClick={this.props.loadSampleBurgers}>
-          Загрузить бургеры
-        </button>
-      </div>
-    );
-  }
+	render() {
+		const {user, photo} = this.state;
+		const avatar = photo ? photo : '/images/avatar.png';
+		return (
+			<div className='menu-admin'>
+				{user ? (
+					<div className='login-header'>
+						<div className='avatar'>
+							<img src={avatar} alt={user}/>
+						</div>
+						<button className='buttonLogout' onClick={this.props.handleLogout}>
+							Выйти
+						</button>
+					</div>
+				) : null}
+				<h2>Управление Меню</h2>
+				{Object.keys(this.props.burgers).map(key => {
+					return (
+						<EditBurgerForm
+							key={key}
+							index={key}
+							deleteBurger={this.props.deleteBurger}
+							updateBurger={this.props.updateBurger}
+							burger={this.props.burgers[key]}
+						/>
+					);
+				})}
+				<AddBurgerForm addBurger={this.props.addBurger}/>
+				<button onClick={this.props.loadSampleBurgers}>
+					Загрузить бургеры
+				</button>
+			</div>
+		);
+	}
 }
 
 export default MenuAdmin;
